0be1c1f80bb29fdcc83e35a477201dd11a9d62de
nelsondavenapalli <nelson.davenapalli@saleshero.ai>
2018-10-17 21:30:34 +0200

    Initial commit with build.gradle

---
+++ src/main/java/com/fun/elevatorDemo/ElevatorDemo/ElevatorDemoApplication.java
@@ -0,0 +1,12 @@
+package com.fun.elevatorDemo.ElevatorDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElevatorDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElevatorDemoApplication.class, args);
+	}
+}
